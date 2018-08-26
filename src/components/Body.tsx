10dfe872fa569e61debfba8edcde1a7f838ba151
Josh McMillan <josh@joshmcmillan.co.uk>
2018-08-26 09:21:37 +0100

    Add bare-bones website

---
+++ src/components/Body.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+export default () => (
+  <Body>
+    Hiya. I'm a software engineer based in London. This site will eventually have more stuff, but for now, this is all.
+  </Body>
+);
+
+const Body = styled.div`
+  padding-top: 1rem;
+  color: #444;
+`;
